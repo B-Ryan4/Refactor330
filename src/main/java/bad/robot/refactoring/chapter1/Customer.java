60115dd19102ee0e9539461d86bf733a32433ddd
Dylan McDonald <dylmcd13@gmail.com>
2022-12-05 14:19:49 -0500

    Add files via upload

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -54,4 +54,15 @@ public class Customer {
             total += rental.getCharge();
         return total;
     }
+    
+    public String htmlStatement() {
+        String result = "<h1>Rental record for <b>" + getName() + "</b></h1>\n";
+        for (Rental rental : rentals)
+            result += "<p>" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "</p>\n";
+        result += "<p>Amount owed is <b>" + String.valueOf(getTotalCharge()) + "</b></p>\n";
+        result += "<p>You earned <b>" + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points</b></p>";
+        return result;
+    }
+    
+    
 }
