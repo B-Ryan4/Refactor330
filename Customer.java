19759582ab1861a67ceb4132bbff7b817f281b22
Dylan McDonald <dylmcd13@gmail.com>
2022-12-05 13:58:30 -0500

    Step 2: Renameing Variables

---
+++ Customer.java
@@ -0,0 +1,68 @@
+package refactor330;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer {
+
+    private String customerName;
+    private List<Rental> rentals = new ArrayList<Rental>();
+
+    public Customer(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getName() {
+        return customerName;
+    }
+
+    public void addRental(Rental rental) {
+        rentals.add(rental);
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+
+        String result = "Rental record for " + getName() + "\n";
+        for (Rental rental : rentals) {
+            
+        	double amount = getAmount(rental);
+            // add frequent renter points
+            frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+                frequentRenterPoints++;
+
+            // show figures for this rental
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+
+            totalAmount += amount;
+        }
+
+        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+
+        return result;
+    }
+    
+    private double getAmount(Rental rental) {
+    	double amount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (rental.getDaysRented() > 2)
+                    amount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (rental.getDaysRented() > 3)
+                    amount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+}
