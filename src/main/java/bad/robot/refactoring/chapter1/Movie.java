3bfe231afd63efccbe4695b9441ebf7bec05b219
Dylan McDonald <dylmcd13@gmail.com>
2022-12-07 11:50:17 -0500

    Step 13 and 14: Replace Conditional

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -7,11 +7,11 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private int priceCode;
+    Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
-        this.priceCode = priceCode;
+        setPriceCode(priceCode);
     }
 
     public String getTitle() {
@@ -19,35 +19,31 @@ public class Movie {
     }
 
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
 
     public void setPriceCode(int priceCode) {
-        this.priceCode = priceCode;
+    	 switch (priceCode) {
+	         case CHILDREN:
+	             price = new Children();
+	             break;
+	         case NEW_RELEASE:
+	             price = new NewRelease();
+	             break;
+	         case REGULAR:
+	             price = new Regular();
+	             break;
+	         default:
+	             throw new IllegalArgumentException("invalid price code");
+    	 }
     }
     
     public double getCharge(int daysRented) {
-        double amount = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case CHILDREN:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
             return 2;
         return 1;
     }
